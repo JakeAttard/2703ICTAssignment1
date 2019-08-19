79e074de1b6d85b069f699aae54cfe4877e6f1ea
www-data <www-data@s5138647.elf.ict.griffith.edu.au>
2019-08-19 23:24:40 +1000

    Created the post form.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-      <title>@yield('title')</title>
+      <title>PostNow -@yield('title')</title>
       <meta charset="utf-8">
       <!-- Boostrap -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/wp.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/styles.css')}}">
     </head>
     
     <body>
@@ -35,8 +35,11 @@
                 </ul>
             </div>
         </nav>
+
+        <div class="backgroundImage"></div>
+
         <div class="contentContainer">
-            @yield('content')
+            @yield('contentContainer')
         </div>
     </body>
 </html>
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.master')
 
 @section('title')
-    PostNow Home Page
+    HomePage
 @endsection
 
 @section('contentContainer')
 
+    <div class="row">
+        
+        <!-- Input Form Starts -->
+        <div class="col-sm-4 form-border">
+            <form method="post" action="">
+                <div class="name form-group"><h3>Create A New Post</h3></div>
+                
+                <div class="name">
+                    <label>User Name: </label><br>
+                    <input type="text" name="name" value="" placeholder="Please enter your name">
+                </div>
+
+                <div class="name"><label>Post Title: </label><br>
+                    <input type="text" name="title" value="" placeholder="Please enter a title">
+                </div>
+
+                <div class="message"><label>Post Message: </label><br>
+                <textarea id="messagetextarea" name="message" rows="4" placeholder="Please enter new message"></textarea>
+                </div>
+
+                <div class="message">
+                    <button class="btn btn-primary" type="submit">POSTNOW</button>
+                </div>
+
+            </form>
+        </div>
+         <!-- Input Form Ends -->      
+    </div>
 @endsection
