f07cb7fa762bf76934883ee5b7959c5d6707ce5f
www-data <www-data@s5138647.elf.ict.griffith.edu.au>
2019-08-26 22:59:41 +1000

    Fake dummy hard coded insert is created for testing purposes.

--- database/postnowdb.sql
+++ database/postnowdb.sql
@@ -1,6 +1,15 @@
+drop table if exists post;
+
 create table post (
-    postId int not null auto_increment primary key,
+    postId int not null primary key,
     postName varchar(30) not null,
     postTitle varchar(50) not null,
     postMessage varchar(200) not null
+);
+
+insert into post values (
+    "",
+    "Jake Attard",
+    "New social media site postnow",
+    "Please share and spread the news that postnow is now global launched."
 );
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -16,15 +16,15 @@
                 
                 <div class="name">
                     <label>User Name: </label><br>
-                    <input type="text" name="postName" value="{{$postName}}" placeholder="Please enter your name">
+                    <input type="text" name="postName" value="" placeholder="Please enter your name">
                 </div>
 
                 <div class="title"><label>Post Title: </label><br>
-                    <input type="text" name="postTitle" value="{{$postTitle}}" placeholder="Please enter a title">
+                    <input type="text" name="postTitle" value="" placeholder="Please enter a title">
                 </div>
 
                 <div class="message"><label>Post Message: </label><br>
-                <textarea id="messagetextarea" name="postMessage" rows="4" placeholder="Please enter new message">{{$postMessage}}</textarea>
+                <textarea id="messagetextarea" name="postMessage" rows="4" placeholder="Please enter new message"></textarea>
                 </div>
 
                 <div class="message">
