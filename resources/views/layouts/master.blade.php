b3beee68e8984b5228116d08fa919d8391577ec4
www-data <www-data@s5138647.elf.ict.griffith.edu.au>
2019-09-01 20:50:50 +1000

    Changes / Fixes.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,7 +15,7 @@
 
     <!-- NavBar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">PostNow</a>
+        <a class="navbar-brand" href="{{url("/")}}">PostNow</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
